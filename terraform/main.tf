4fd1df4eea420a99600ea80307b63056639d9b9e
Mahad Mohamed <tecknosap@gmail.com>
2025-08-18 21:57:27 +0100

    initail commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,13 +3,20 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
+
+
+resource "azurerm_resource_group" "rg" {
+  name = var.resource_group
+  location = var.location
+  
+}
 # ===============================
 # Create an Azure Container Registry (ACR) in the specified resource group and region
 # ===============================
 resource "azurerm_container_registry" "acr" {
   name                = "tecknoacr"
-  location            = var.location
-  resource_group_name = var.resource_group
+  location            = azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.rg.location
   sku                 = "Basic"
   admin_enabled       = false
 }
