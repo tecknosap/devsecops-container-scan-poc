eb32926a2b2c60f6c86e3d4bc1f2f48208afb2de
Mahad Mohamed <tecknosap@gmail.com>
2025-08-18 14:37:45 +0100

    initail commit

---
+++ terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "rg-gocloudops-terraform-backend"
+    storage_account_name  = "gocloudopstfstate"
+    container_name        = "tfstate"
+    key                   = "devsecops-poc2.tfstate"
+  }
+}
