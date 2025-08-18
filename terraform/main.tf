a5d4a30f784a9a33df3ad14f85f4efd98f815fe1
Mahad Mohamed <tecknosap@gmail.com>
2025-08-18 14:39:00 +0100

    initail commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "4.40.0"
-    }
-  }
-}
-
 provider "azurerm" {
   # Configuration options
   features {
