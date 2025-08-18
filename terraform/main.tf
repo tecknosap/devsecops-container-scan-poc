7083b91c7819ede46e36c36398c3012330fe6338
Mahad Mohamed <tecknosap@gmail.com>
2025-08-18 22:08:48 +0100

    initail commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,8 +15,8 @@ resource "azurerm_resource_group" "rgs" {
 # ===============================
 resource "azurerm_container_registry" "acr" {
   name                = "tecknoacr"
-  location            = azurerm_resource_group.rgs.name
-  resource_group_name = azurerm_resource_group.rgs.location
+  location            = azurerm_resource_group.rgs.location
+  resource_group_name = azurerm_resource_group.rgs.name
   sku                 = "Basic"
   admin_enabled       = false
 }
